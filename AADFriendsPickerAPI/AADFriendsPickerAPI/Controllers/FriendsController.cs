19c98e4e15b6d0461134473671b6d8b5e58332c6
DalexKraus <dalex.kraus@gmail.com>
2021-03-11 12:29:25 +0100

    More work

--- AADFriendsPickerAPI/AADFriendsPickerAPI/Controllers/FriendsController.cs
+++ AADFriendsPickerAPI/AADFriendsPickerAPI/Controllers/FriendsController.cs
@@ -16,6 +16,8 @@ namespace AADFriendsPickerAPI.Controllers
     {
         private readonly FriendsDbContext _context;
         public FriendsController(FriendsDbContext context) => _context = context;
+        
+        public record AddFriendModel(string FriendId);
 
         [Route("getAll")]
         [HttpGet]
@@ -23,8 +25,41 @@ namespace AADFriendsPickerAPI.Controllers
         public async Task<IEnumerable<string>> GetAll()
         {
             var userId = HttpContext.User.Claims.First(c => c.Type == ClaimConstants.ObjectId).Value;
-            var friendIds = await _context.Friendships.Where(f => f.FirstUserId == userId || f.SecondUserId == userId).ToArrayAsync();
-            return friendIds.Select(f => (f.FirstUserId == userId) ? f.SecondUserId : f.FirstUserId);
+            var friendShips = await _context.Friendships.Where(f => f.FirstUserId == userId || f.SecondUserId == userId).ToArrayAsync();
+            var friendIds = friendShips.Select(f => (f.FirstUserId == userId) ? f.SecondUserId : f.FirstUserId);
+            return friendIds;
+        }
+
+        [Route("isFriend")]
+        [HttpPost]
+        [Authorize]
+        public async Task<bool> IsFriend([FromBody] string friendId)
+        {
+            return (await GetAll()).Any(f => f == friendId);
+        }
+        
+        [Route("add")]
+        [HttpPost]
+        [Authorize]
+        public async Task<IEnumerable<string>> AddFriend([FromBody] AddFriendModel model)
+        {
+            var friendId = model.FriendId;
+            // Check if the user is not already friends with the given person
+            if (!await IsFriend(friendId))
+            {
+                await using var transaction = await _context.Database.BeginTransactionAsync();
+                var friendShip = new Friendship
+                {
+                    FirstUserId = HttpContext.User.Claims.First(c => c.Type == ClaimConstants.ObjectId).Value,
+                    SecondUserId = friendId
+                };
+                await _context.Friendships.AddAsync(friendShip);
+            
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+
+            return await GetAll();
         }
     }
 }
--- AadFriendsPicker/src/app/app.component.ts
+++ AadFriendsPicker/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { MsalService } from "@azure/msal-angular";
 import { AuthenticationResult } from "@azure/msal-browser";
 
 import * as MicrosoftGraph from "@microsoft/microsoft-graph-types"
+import { environment } from "src/environments/environment";
 
 interface IODataResult<T> {
     value: T;
+}
+
+class AddFriendModel {
+    constructor(private friendId: string) { }
 }
 
 @Component({
@@ -19,8 +24,11 @@ export class AppComponent implements OnInit {
     loggedIn = false;
     profile!: MicrosoftGraph.User;
 
-    userSearchFilter: String = "";
+    userSearchFilter: string = "";
+    addFriendSearchFilter: string = "";
     users!: MicrosoftGraph.User[];
+    friends: Array<MicrosoftGraph.User> = new Array<MicrosoftGraph.User>();
+    friendIds!: any;
 
     constructor(private authService: MsalService, private client: HttpClient) {}
 
@@ -31,6 +39,7 @@ export class AppComponent implements OnInit {
     async checkAccount() {
         if (this.authService.instance.getAllAccounts().length > 0) {
             this.profile = await this.getProfileInformation();
+            await this.updateFriends();
             this.loggedIn = true;
         }
     }
@@ -52,17 +61,53 @@ export class AppComponent implements OnInit {
         return await this.client.get("https://graph.microsoft.com/v1.0/me").toPromise();
     }
 
-    getUsers() {
+    async updateUsers(searchFilter: string) {
         let params = new HttpParams().set("$top", "10");
         if (this.userSearchFilter) {
           params = params.set(
             "$filter",
-            `startsWith(displayName, '${this.userSearchFilter}')`
+            `startsWith(displayName, '${searchFilter}')`
           );
         }
         let url = `https://graph.microsoft.com/v1.0/users?${params.toString()}`;
-        this.client
-          .get<IODataResult<MicrosoftGraph.User[]>>(url)
-          .subscribe((users) => (this.users = users.value));
-      }
+        this.users = (await this.client.get<IODataResult<MicrosoftGraph.User[]>>(url).toPromise()).value;
+    }
+
+    private async lookupUser(userName: string): Promise<MicrosoftGraph.User>
+    {
+        let params = new HttpParams().set("$top", "1");
+        if (this.userSearchFilter) {
+          params = params.set(
+            "$filter",
+            `startsWith(displayName, '${userName}')`
+          );
+        }
+        let url = `https://graph.microsoft.com/v1.0/users?${params.toString()}`;
+        return (await this.client.get<IODataResult<MicrosoftGraph.User[]>>(url).toPromise()).value![0];
+    }
+
+    async updateFriends() {
+        // Retrieve all friend ids
+        this.friendIds = await this.client.get<string[]>(environment.customApi + "/getAll").toPromise();
+        
+        // Clear all previously looked up friends
+        if (this.friends != null)
+            this.friends.length = 0; 
+
+        // Lookup each id with their corresponding name
+        for (const friendId of this.friendIds) {
+            let userData = await this.lookupUser(friendId);
+            this.friends.push(userData);
+        }
+    }
+    
+    async addFriend(user: MicrosoftGraph.User) {
+        var model = new AddFriendModel(user!.id!.toString());
+        this.friendIds = await this.client.post<AddFriendModel>(environment.customApi + "/add", model).toPromise();
+        this.userSearchFilter = "";
+    }
+
+    public isFriend(user: MicrosoftGraph.User): boolean {
+        return this.profile.id == user.id && this.friendIds != null && this.friendIds.indexOf(user!.id) != -1;
+    }
 }
